ff4e1ded3a53a867da85e65c31af12b57fed7deb
darbyredhat <cdarby@redhat.com>
2020-10-22 13:50:44 -0400

    vets-service baseline

--- quarkus-petclinic-vets-service/src/main/java/org/acme/rest/VetsResource.java
+++ quarkus-petclinic-vets-service/src/main/java/org/acme/rest/VetsResource.java
@@ -1,14 +1,10 @@
 package org.acme.rest;
 
-import java.util.List;
-
 import javax.inject.Inject;
-import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.acme.model.Vet;
 import org.acme.service.VetsService;
 import org.jboss.logging.Logger;
 
@@ -21,9 +17,4 @@ public class VetsResource {
     @Inject
     VetsService service;
 
-    @GET
-    public List<Vet> get() {
-        LOG.debug("Inside get() method");
-        return service.getAll();
-    }
 }
