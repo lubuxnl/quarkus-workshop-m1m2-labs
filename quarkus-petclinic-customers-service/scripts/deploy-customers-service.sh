0d1ead6b79fe7e6131277b83a2aab4de6b0d1b92
darbyredhat <cdarby@redhat.com>
2020-10-23 15:53:45 -0400

    update

--- quarkus-petclinic-customers-service/scripts/deploy-customers-service.sh
+++ quarkus-petclinic-customers-service/scripts/deploy-customers-service.sh
@@ -5,8 +5,6 @@ echo Deploy customers-service........
 cd /projects/quarkus-workshop-labs/quarkus-petclinic-customers-service
 
 # oc delete deployments,dc,bc,build,svc,route,pod,is --all
-# echo "Waiting 30 seconds to finalize deletion of resources..."
-# sleep 30
 
 mvn clean package -DskipTests
 
