830ab88b1d8e284b52deea62790027be3cabb324
darbyredhat <cdarby@redhat.com>
2020-10-24 15:33:33 -0400

    updates for vets-service

--- quarkus-petclinic-vets-service/scripts/deploy-vets-service.sh
+++ quarkus-petclinic-vets-service/scripts/deploy-vets-service.sh
@@ -2,6 +2,8 @@
 
 echo Deploy vets-service........
 
+oc delete deployments,dc,bc,build,svc,route,pod,is vets-service
+
 cd /projects/quarkus-workshop-labs/quarkus-petclinic-vets-service
 
 mvn clean package -DskipTests
@@ -9,27 +11,21 @@ mvn clean package -DskipTests
 #
 # Database
 #
-oc new-app -e POSTGRESQL_USER=vets \
-  -e POSTGRESQL_PASSWORD=mysecretpassword \
-  -e POSTGRESQL_DATABASE=vets openshift/postgresql:latest \
-  --name=vets-database
+oc new-app openshift/postgresql:latest \
+            --name=vets-database \
+            -e POSTGRESQL_USER=vets \
+            -e POSTGRESQL_PASSWORD=mysecretpassword \
+            -e POSTGRESQL_DATABASE=vets 
 
 #
 # Quarkus App
 #
-oc new-build registry.access.redhat.com/openjdk/openjdk-11-rhel7 --binary --name=vets-service -l app=vets-service
-oc start-build vets-service --from-file=target/quarkus-petclinic-vets-service-1.0.0-SNAPSHOT-runner.jar --follow
-oc new-app vets-service -e QUARKUS_PROFILE=prod
-oc expose service vets-service
+mvn clean package -Dquarkus.kubernetes.deploy=true
 
-clear
 echo "Done! Verify by using steps below:"
 echo
 echo "Run the curl command to view a list of vets (json):"
 echo "$ curl http://$(oc get route vets-service -o=go-template --template='{{ .spec.host }}')/vets"
-echo
-echo "Open a web browser and visit the URL to view a list of vets (json):"
-echo "http://$(oc get route vets-service -o=go-template --template='{{ .spec.host }}')/vets"
 echo
 echo "Open a web browser and visit Swagger UI"
 echo "http://$(oc get route vets-service -o=go-template --template='{{ .spec.host }}')/swagger-ui"
